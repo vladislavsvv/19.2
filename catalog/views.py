088552245324a481819ecf491ea1e86cdba1f87d
vladislav <vladisla.sv.299563@gmail.com>
2023-10-26 14:35:52 +0600

    add homework 19.2

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(requests):
+    return render(requests, 'catalog/index.html')
+
+
+def contacts(requests):
+    if requests.method == 'POST':
+        name = requests.POST.get('name')
+        email = requests.POST.get('email')
+        subject = requests.POST.get('subject')
+        message = requests.POST.get('message')
+
+        print(f"{name}, ({email}) says: {subject}\n{message}")
+    return render(requests, 'catalog/contacts.html')
